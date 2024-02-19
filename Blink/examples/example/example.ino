8a1311b4d11cdfa3bc2819435c0604cd721b8ed5
Khangvi753 <160449963+Khangvi753@users.noreply.github.com>
2024-02-19 15:46:23 +0700

    Delete Blink directory

--- Blink/examples/example/example.ino
+++
@@ -1,9 +0,0 @@
-#include "Blink.h"
-Blink Blink(2);  // Blink(pin connected)
-void setup() {
-  Blink.begin();
-}
-
-void loop() {
-  Blink.BlinkLED(2000, 5000);  // BlinkLED(time LED on , time LED off)
-}
